1e5dec2a060b3484e47cbc272e5523ebb121763d
zawadall <zakiawadallah.04@gmail.com>
2024-05-02 22:43:55 +0300

    page 2

--- main.go
+++ main.go
@@ -5,18 +5,24 @@ import (
 	"net/http"
 )
 
-var temp *template.Template
+var temp, temp2 *template.Template
 
 func main() {
 	call()
 	temp = template.Must(template.ParseGlob("tracker.html"))
+	temp2 = template.Must(template.ParseGlob("artist.html"))
 	fs := http.FileServer(http.Dir("style"))
 	http.Handle("/style/", http.StripPrefix("/style", fs))
 	http.HandleFunc("/", MainHandler)
+	http.HandleFunc("/artist/", ArtistHandler)
 	http.ListenAndServe(":8080", nil)
-	
+
 }
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "tracker.html", Data)
+}
+
+func ArtistHandler(w http.ResponseWriter, r *http.Request) {
+	temp2.ExecuteTemplate(w, "artist.html", Data)
 }
